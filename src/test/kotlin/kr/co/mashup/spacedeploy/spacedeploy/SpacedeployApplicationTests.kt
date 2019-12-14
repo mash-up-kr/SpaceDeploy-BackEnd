42af41081e904f0b4a49d9259fc1b161da8f90c3
jercy <wotjdzz1@naver.com>
2019-12-14 20:32:50 +0900

    testcase 수정

--- src/test/kotlin/kr/co/mashup/spacedeploy/spacedeploy/SpacedeployApplicationTests.kt
+++ src/test/kotlin/kr/co/mashup/spacedeploy/spacedeploy/SpacedeployApplicationTests.kt
@@ -1,6 +1,6 @@
 package kr.co.mashup.spacedeploy.spacedeploy
 
-import kr.co.mashup.spacedeploy.spacedeploy.user.sendGet
+import kr.co.mashup.spacedeploy.spacedeploy.user.getUserInfo
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
