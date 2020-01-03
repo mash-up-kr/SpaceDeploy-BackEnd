6f482c491ead0c113a5c0c1bd61fa5a90b531872
jercy <wotjdzz1@naver.com>
2020-01-04 05:33:40 +0900

    cors 적용

--- src/main/kotlin/kr/co/mashup/spacedeploy/spacedeploy/login/LoginController.kt
+++ src/main/kotlin/kr/co/mashup/spacedeploy/spacedeploy/login/LoginController.kt
@@ -5,12 +5,14 @@ import io.swagger.annotations.ApiOperation
 import kr.co.mashup.spacedeploy.spacedeploy.oauth.getUID
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 
 @Api(description = "로그인 API")
+@CrossOrigin
 @RestController
 class LoginController(val loginService: LoginService) {
     @ApiOperation(value = "로그인", notes = "로그인")
