1ad0bbf533fadcb9fabe66caae1f06ba281625f4
jercy <wotjdzz1@naver.com>
2020-01-26 20:36:09 +0900

    테스트 추가

--- src/test/kotlin/kr/co/mashup/spacedeploy/spacedeploy/SpacedeployApplicationTests.kt
+++ src/test/kotlin/kr/co/mashup/spacedeploy/spacedeploy/SpacedeployApplicationTests.kt
@@ -6,19 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class SpacedeployApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-    @Test
-    fun kakaoTokenTest() {
-        getUID("5pg4NUK6zATK5LKmiKvH94P3dvynGAqTeSW6xAopdXYAAAFvBCdOZQ", "kakao")
-        assert(true)
-    }
-
-    fun facebookTokenTest() {
-        getUID("EAAMYTVEn0OABAOxZBDvKtcq19bFsZB4GiQ5dJZCnfKbdGP6Ob4xMVdpZBOa4iH0Jjk8g79hodqGcpv8ycZB08ZBKSJti0ctt5nTOz58fRJjESZBZB8IRIp6mniwz0V03Uh4Ngve94zirs6XExboUVmsBOhzIwDcsCNFZA6YZBTUHdgwgZDZD", "facebook")
-    }
-
 }
